1e25ceff565b6dd5d648c6acca0c50f648a4965d
webintellectual <202051018@iiitvadodara.ac.in>
2022-10-15 16:29:03 +0530

    add media

---
+++ 08_Media/Readme.md
@@ -0,0 +1,5 @@
+- st.image
+- st.audio
+- st.video
+
+<img src="output.png" alt="display" width=800/>
---
+++ 08_Media/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+path="08_Media/"
+st.image(path+"image.jpg")
+st.audio(path+"audio.mp3")
+st.video("https://youtu.be/AGtP0M0qdWM")
